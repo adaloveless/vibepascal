29bea0193935adbbf0c3a0b4693afce9df670572
fibodevy <fibodevy@gmail.com>
2026-03-18 17:40:15 +0100

    Add missing `m_inline_var` to ppudump modeswitchname table

--- compiler/utils/ppuutils/ppudump.pp
+++ compiler/utils/ppuutils/ppudump.pp
@@ -2487,7 +2487,8 @@ const
          'm_anonymous_functions', { enable Delphi-style anonymous functions }
          'm_statement_expressions', { enables expressions using statements like if, case, try }
          'm_array_equality',      { enables equality operator in addition to ArrayOperators modeswitch }
-         'm_no_rtti'              { hides RTTI ASCII text }
+         'm_no_rtti',             { hides RTTI ASCII text }
+         'm_inline_var'           { allow inline variable declarations inside statement blocks }
        );
        { optimizer }
        optimizerswitchname : array[toptimizerswitch] of string[50] =
