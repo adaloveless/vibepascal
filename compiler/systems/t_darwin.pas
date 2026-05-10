351d652f482264e8b685390db1c8a672dfe986bf
adaloveless <ada@digitaltundra.com>
2026-05-10 21:20:10 +0000

    darwin: use modern macOS linker min-version flag

--- compiler/systems/t_darwin.pas
+++ compiler/systems/t_darwin.pas
@@ -364,9 +364,9 @@ implementation
            { This does not depend on the target version but on the toolchain
              version, but we only know the former and not the latter }
            if MacOSXVersionMin.relationto(11,0,0)>=0 then
-             result:='-macosx_version_min '+MacOSXVersionMin.str
+             result:='-macos_version_min '+MacOSXVersionMin.str
            else
-             result:='-macos_version_min '+MacOSXVersionMin.str;
+             result:='-macosx_version_min '+MacOSXVersionMin.str;
           end
         else if iPhoneOSVersionMin.isvalid then
           begin
@@ -808,4 +808,3 @@ initialization
   RegisterRes(res_macho_info,TWinLikeResourceFile);
   RegisterRes(res_macosx_ext_info,TWinLikeResourceFile);
 end.
-
